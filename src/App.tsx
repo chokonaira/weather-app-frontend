7507f4123fe07578c4a828abfcafb31352be01d4
Henry Okonkwo <okonkwo.henry2012@gmail.com>
2021-10-05 11:32:33 +0200

    setup react project with typescript remove unwanted files

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './assets/styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Weather App</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
