39620ca02f860635ac819857e089e3503656d0dd
Henry Okonkwo <okonkwo.henry2012@gmail.com>
2021-10-06 22:40:31 +0200

    Add cors configuration in server.js to enable cors

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
-const express = require('express');
-const path = require('path');
+const path = require("path");
+const express = require("express");
+const cors = require("cors");
+
 const app = express();
+app.use(cors());
 
 const port = process.env.PORT || 5000;
 
-app.use(express.static(path.join(__dirname, 'build')));
+const publicPath = path.join(__dirname, ".", "build");
+app.use(express.static(publicPath));
 
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+app.get("/", (req, res) => {
+  res.set('Access-Control-Allow-Origin', '*');
+  res.sendFile(path.join(publicPath, "index.html"));
 });
-
 app.listen(port, () => {
   console.log(`Up and running on port ${port}`);
 });
