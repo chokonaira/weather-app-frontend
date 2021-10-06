2585b1dc55587fc6febd3392cd250db84cb9946b
Henry Okonkwo <okonkwo.henry2012@gmail.com>
2021-10-07 00:06:22 +0200

    Add cors bypass url axios instance to enable cors

--- server.js
+++ server.js
@@ -11,7 +11,6 @@ const publicPath = path.join(__dirname, ".", "build");
 app.use(express.static(publicPath));
 
 app.get("*", (req, res) => {
-  res.set('Access-Control-Allow-Origin', 'http://api.openweathermap.org/data/2.5/forecast');
   res.sendFile(path.join(publicPath, "index.html"));
 });
 app.listen(port, () => {
