291d0f449bf970b41d648cf09918a15ba771007e
Henry Okonkwo <okonkwo.henry2012@gmail.com>
2021-10-08 13:37:57 +0200

    abstarct reuseable convertTemperatureHelper function and refactor test to follow DRY principle

---
+++ src/__test__/testHelper.ts
@@ -0,0 +1,42 @@
+export const convertTemperatureHelper = (scale: string, convertedTemperature: string) => {
+  const newScale = scale;
+  const weatherData = {
+    city: {
+      name: "Munich",
+    },
+    list: [
+      {
+        main: {
+          temp: 234.4,
+        },
+        dt_txt: "2021-10-06 21:00:00",
+        weather: [
+          {
+            id: 1,
+            main: "Clouds",
+            description: "scattered clouds",
+            icon: "icon",
+          },
+        ],
+      },
+    ],
+  };
+
+  const dispatchedData = {
+    data: weatherData,
+    scale,
+    city: "Munich",
+    newWeatherData: [
+      {
+        id: 1,
+        main: "Clouds",
+        icon: "icon",
+        convertedTemperature,
+        date: "2021-10-06 21:00:00",
+        description: "scattered clouds",
+      },
+    ],
+  };
+
+  return { newScale, weatherData, dispatchedData };
+}
--- src/__test__/weather-action.test.ts
+++ src/__test__/weather-action.test.ts
@@ -3,50 +3,16 @@ import { convertTemperatures } from "../redux/actions/weather";
 import configureStore from "redux-mock-store";
 import thunk from "redux-thunk";
 import { WEATHER_SUCCESS } from "../redux/actions/types";
+import { convertTemperatureHelper } from './testHelper';
+
 
 const mockStore = configureStore([thunk]);
 
 describe("weather", () => {
   it("converts kelvin temperature value to its celcius value and dispatches to redux state", (done) => {
     const store = mockStore({});
-    const newScale = "celcius";
-    const weatherData = {
-      city: {
-        name: "Munich",
-      },
-      list: [
-        {
-          main: {
-            temp: 234.4,
-          },
-          dt_txt: "2021-10-06 21:00:00",
-          weather: [
-            {
-              id: 1,
-              main: "Clouds",
-              description: "scattered clouds",
-              icon: "icon",
-            },
-          ],
-        },
-      ],
-    };
+    const { newScale, weatherData, dispatchedData } = convertTemperatureHelper('celcius', '-39°C');
 
-    const dispatchedData = {
-      data: weatherData,
-      scale: "celcius",
-      city: "Munich",
-      newWeatherData: [
-        {
-          id: 1,
-          main: "Clouds",
-          icon: "icon",
-          convertedTemperature: "-39°C",
-          date: "2021-10-06 21:00:00",
-          description: "scattered clouds",
-        },
-      ],
-    };
     const expectedActions = [
       {
         type: WEATHER_SUCCESS,
@@ -61,44 +27,9 @@ describe("weather", () => {
 
   it("converts kelvin temperature value to its fahrenheit value and dispatches to redux state", (done) => {
     const store = mockStore({});
-    const newScale = "fahrenheit";
-    const weatherData = {
-      city: {
-        name: "Munich",
-      },
-      list: [
-        {
-          main: {
-            temp: 234.4,
-          },
-          dt_txt: "2021-10-06 21:00:00",
-          weather: [
-            {
-              id: 1,
-              main: "Clouds",
-              description: "scattered clouds",
-              icon: "icon",
-            },
-          ],
-        },
-      ],
-    };
+    const { newScale, weatherData, dispatchedData } = convertTemperatureHelper('fahrenheit', '-38°F');
 
-    const dispatchedData = {
-      data: weatherData,
-      scale: "fahrenheit",
-      city: "Munich",
-      newWeatherData: [
-        {
-          id: 1,
-          main: "Clouds",
-          icon: "icon",
-          convertedTemperature: "-38°F",
-          date: "2021-10-06 21:00:00",
-          description: "scattered clouds",
-        },
-      ],
-    };
+  
     const expectedActions = [
       {
         type: WEATHER_SUCCESS,
